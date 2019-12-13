a2287da47202ac04932fd04bcf21d8cae511aacf
Md Adil <md-adil@live.com>
2019-12-13 13:15:55 +0530

    inital commit

---
+++ src/Model.ts
@@ -0,0 +1,51 @@
+import Axios from "axios";
+import Pagination from "./Pagination";
+
+class Model<D> {
+    static collection: string;
+    static pk: string = "id";
+
+    static async create(props: any) {
+        const { data } = await Axios.post(`${this.collection}`, props);
+        return new this(data);
+    }
+
+    static paginate<T>(queries?: any) {
+        return Pagination.create<T>(this, queries);
+    }
+
+    static async fetch(id: number) {
+        const { data } = await Axios.get(`${this.collection}/${id}`);
+        return new this(data);
+    }
+
+    static async fetchAll<T>(queries?: any) {
+        const { data } = await Axios.get<T[]>(`${this.collection}`, {
+            params: queries
+        });
+        return data.map((r) => new this(r));
+    }
+
+    static async fetchPagination(page: number, queries?: any) {
+        const { data } = await Axios.get(`${this.collection}`, {
+            params: { page, ...queries }
+        });
+        data.data = data.data.map((d: any) => new this(d));
+        return data;
+    }
+
+    constructor(public readonly attributes: D) {
+    }
+
+    async update(attributes: Partial<D>) {
+        const { data } = await Axios.put<D>(`${(this.constructor as any).collection}/${this.id}`);
+        return data;
+    }
+
+    get id() {
+        const M = this.constructor as any;
+        return (this.attributes as any)[M.pk];
+    }
+}
+
+export default Model;
---
+++ src/Pagination.ts
@@ -0,0 +1,47 @@
+export interface IPagination<T> {
+    data: T[];
+    current_page: number;
+    per_page: number;
+    total: number;
+}
+
+class Pagination<T> {
+    static async create<T>(model: any, page: number, queries?: any) {
+        const data = await model.fetchPagination(page, queries);
+        return new this<T>(model, data, queries);
+    }
+    page = 0;
+    total = 0;
+    results = 0;
+    data: T[];
+    totalPage = 0;
+    constructor(
+        public readonly model: any,
+        data: IPagination<T>,
+        public queries?: any
+    ) {
+        this.data = data.data.map(d => new this.model(d));
+        this.page = data.current_page;
+        this.results = data.per_page;
+        this.total = data.total;
+        this.totalPage = Math.ceil(data.total / data.per_page);
+    }
+
+    nextPage<T>() {
+        return Pagination.create<T>(this.model, this.page + 1, this.queries)
+    }
+
+    prevPage<T>() {
+        return Pagination.create<T>(this.model, this.page - 1, this.queries);
+    }
+
+    get hasNext() {
+        return this.page < this.totalPage;
+    }
+
+    get hasPrev() {
+        return this.page > 1;
+    }
+}
+
+export default Pagination;
